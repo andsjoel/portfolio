2667754fa4b7947c40f7e26c701236f2f1522567
andsjoel <domnquo@gmail.com>
2024-01-24 09:45:38 -0300

    section 3 - Skills

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Apresentation from "./1_Apresentation/Apresentation";
 import AboutMe from "./2_AboutMe/AboutMe";
 import Footer from "../components/Footer/Footer";
+import Skills from "./3_Skills/Skills";
 
 const Home = () => {
     return (
         <main>
             <Apresentation />
             <AboutMe />
+            <Skills />
             <Footer />
         </main>
     );
