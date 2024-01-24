d56af52c72ddf86bde7b3809c8782bbe1ae31eac
andsjoel <domnquo@gmail.com>
2024-01-24 09:24:26 -0300

    section 1 - apresentation

---
+++ src/sections/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Apresentation from "./sect1/Apresentation";
+
+const Home = () => {
+    return (
+        <main>
+            <Apresentation />
+        </main>
+    );
+}
+
+export default Home;
---
+++ src/sections/sect1/Apresentation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./apresentation.css"
+
+const Apresentation = () => {
+    return (
+        <section className="apresentation">
+            <div>
+                <div className="myName">
+                    <p>Olá, meu nome é</p>
+                    <h2>Anderson Joel</h2>
+                </div>
+                <div className="myName">
+                    <p>mas pode me chamar de</p>
+                    <h2>Domenico</h2>
+                    <p>se preferir.</p>
+                </div>
+            </div>
+            <div className="itsMe">
+                <span>Admirado pelo futuro e apaixonado por tecnologias.</span>
+                <p>Sou um desenvolver web de front-end, back-end e estudante de</p>
+                <p>Inteligência Artificial.</p>
+            </div>
+        </section>
+    );
+}
+
+export default Apresentation;
